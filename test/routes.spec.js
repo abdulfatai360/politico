8152b24dc6da31f9eeb1b94d855c0ce23f504a24
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-02-02 12:36:10 +0100

    [Feature #163671466] Add routing for undefined endpoints

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,6 +7,49 @@ import officeDb from '../src/models/office';
 const { expect } = chai;
 chai.use(chaiHttp);
 
+/* ****** Undefined route ******* */
+describe('Undefined Route', () => {
+  it('should return 404 for GET', () => {
+    chai.request(server)
+      .get('/api/v1/anyRoute')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('should return 404 for POST', () => {
+    chai.request(server)
+      .post('/api/v1/anyRoute')
+      .send({
+        name: 'John Doe',
+        hqAddress: '12, Empty Street, Cul de Sacs, NG',
+        logoUrl: 'https://example.com',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('should return 404 for PATCH', () => {
+    chai.request(server)
+      .patch('/api/v1/anyRoute')
+      .send({
+        name: 'John Doe',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('should return 404 for DELETE', () => {
+    chai.request(server)
+      .delete('/api/v1/anyRoute')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+      });
+  });
+});
+
 /* ****** Feature: Create a party ******* */
 describe('POST /api/v1/parties', () => {
   describe('On successful request/response cycle', () => {
