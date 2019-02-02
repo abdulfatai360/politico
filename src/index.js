88e837cf188e96801124ca314a7671d00797a6f5
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-02-02 12:18:53 +0100

    [Feature #163671207] Add GET /offices API

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import officeRouter from './routes/office';
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
 
 app.use('/api/v1/parties', partyRouter);
 app.use('/api/v1/offices', officeRouter);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is ready at port ${port}`);
 });
